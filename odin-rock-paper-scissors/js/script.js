d0f67feb3e2c6cd949226fb18dde33b6b06fcc64
knaub <eknaub404@gmail.com>
2022-08-18 14:49:18 +0200

    rockpaperscissors project ugly ui

--- odin-rock-paper-scissors/js/script.js
+++ odin-rock-paper-scissors/js/script.js
@@ -1,38 +1,86 @@
-game();
+const gameScreen = document.querySelector('.game-screen');
+const btnsDiv = document.querySelector('.player-selection');
+const btns = document.querySelectorAll('.player-selection-btn');
+btns.forEach((button) => {
+  const playerSelection = button.classList[0];
+  button.addEventListener('click', function() {
+    playRound(playerSelection)
+  });
+})
+const result = document.querySelector('.result');
+const score = document.querySelector('.score');
 
-function game() {
-  let playerWins = 0;
-  let computerWins = 0;
+const winScreen = document.querySelector('.win-screen');
+const winner = document.querySelector('.winner');
+const playAgain = document.querySelector('.play-again');
+playAgain.addEventListener('click', restartGame);
 
-  for(let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Rock, paper or scissors?");
-    const computerSelection = getComputerChoice();
-    const playerWon = playGame(playerSelection, computerSelection);
+let playerWins = 0;
+let computerWins = 0;
+const MAXWINS = 5;
 
-    if(playerWon != undefined) {
-      playerWon ? playerWins++ : computerWins++;
-      console.log(`PlayerWins ${playerWins}`);
-      console.log(`ComputerWins ${computerWins}`);
-    }
-  }
+startGame();
+
+function startGame() {
+  resetScores();
+  showGameScreen();
+}
+
+function restartGame() {
+  startGame();
+}
+
+function showGameScreen() {
+  winScreen.style.display = "none";
+  gameScreen.style.display = "block";
+}
+
+function showResultScreen() {
+  winScreen.style.display = "block";
+  gameScreen.style.display = "none";
+}
+
+function resetScores() {
+  playerWins = 0;
+  computerWins = 0;
+  result.textContent = '';
+  score.textContent = `Score: Computer 0 - Player 0`;
 }
 
 function getComputerChoice() {
-  const choices = ['rock', 'paper', 'scissors'];
+  const choices = new Array('rock', 'paper', 'scissors');
   const choice = Math.floor((Math.random() * 3));
 
   return choices[choice];
 }
 
-function playGame(playerSelection, computerSelection) {
+function playRound(playerSelection) {
   const ps = playerSelection.toLowerCase();
-  const cs = computerSelection.toLowerCase();
+  const cs = getComputerChoice().toLowerCase();
 
   const playerWon = checkIfPlayerWon(ps,cs);
-  
-  console.log(buildMessage(playerWon, ps, cs));
 
-  return playerWon;
+  if(playerWon != undefined) {
+    playerWon ? playerWins++ : computerWins++;
+  }
+  
+  if(playerWins < MAXWINS && computerWins < MAXWINS) {
+    score.textContent = `Score: Computer ${computerWins} - Player ${playerWins}`;
+    result.textContent = `Result: ${buildResultMessage(playerWon, ps, cs)}`;
+  }
+  else {
+    showWinner();
+  }
+}
+
+function showWinner() {
+  showResultScreen();
+  if(playerWins === MAXWINS) {
+    winner.textContent = 'Player won! :)';
+  }
+  else {
+    winner.textContent = 'Computer won! :(';
+  }
 }
 
 function checkIfPlayerWon(ps, cs) {
@@ -79,7 +127,7 @@ function checkIfPlayerWon(ps, cs) {
   return playerWon;
 }
 
-function buildMessage(playerWin, playerSelection, computerSelection) {
+function buildResultMessage(playerWin, playerSelection, computerSelection) {
   if(playerWin == undefined) {
     return `It\'s a tie with ${playerSelection}!`;
   }
